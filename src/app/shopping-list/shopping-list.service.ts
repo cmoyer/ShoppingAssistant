a308a48d0c874ea0c4a89f09466ebde4b953233f
Chad Moyer <c.d.moyer521@gmail.com>
2017-04-18 14:34:51 -0400

    Created a ShoppingListService to handle data related to our shopping list components and to remove the complexity of all of the @Input/@Output listeners. I've imported the service in the app.module because we will want to use it globally to include a shopping list with a recipe.

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,20 @@
-export class ShoppingListService {
+import {Ingredient} from '../shared/ingredient.model';
+import {EventEmitter} from '@angular/core';
 
+export class ShoppingListService {
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
+  private ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10)
+  ];
+
+  getIngredients() {
+    // use the .slice() method to return a copy of our private array ingredients rather than returning our actual array.
+    return this.ingredients.slice();
+  }
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    this.ingredientsChanged.emit(this.ingredients.slice());
+  }
 }
