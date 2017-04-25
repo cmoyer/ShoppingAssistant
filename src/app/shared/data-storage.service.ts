617ca5e9b810b2d93995b71f35e4ea67e617c5a7
Chad Moyer <c.d.moyer521@gmail.com>
2017-04-25 11:01:59 -0400

    Fixed an issue where Recipes with no ingredients were stored without the ingredients property on our backend.

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
+import 'rxjs/Rx';
 
 @Injectable()
 export class DataStorageService {
@@ -13,9 +14,20 @@ export class DataStorageService {
 
   getRecipes() {
     this.http.get('https://ng-recipe-book-8a15b.firebaseio.com/recipes.json')
-      .subscribe(
+      .map(
         (response: Response) => {
           const recipes: Recipe[] = response.json();
+          for (let recipe of recipes) {
+            if (!recipe['ingredients']) {
+              console.log(recipe);
+              recipe['ingredients'] = [];
+            }
+          }
+          return recipes;
+        }
+      )
+      .subscribe(
+        (recipes: Recipe[]) => {
           this.recipeService.setRecipes(recipes);
         }
       );
